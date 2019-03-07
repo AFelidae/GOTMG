3f008c4d7f9a2c9fb7aa8734e98e0845c09d42d7
AnotherEntity <prteza@gmail.com>
2019-03-06 22:55:16 -0800

    Housekeeping/Prepwork

--- setup.js
+++ setup.js
@@ -8,42 +8,31 @@ addEventListener("keyup", function (e) {
   delete keysDown[e.keyCode];
 }, false)
 
-var camera, scene, renderer;
-var mesh;
+var camera, scene;
 
-var scale = 100
-
-var startPos = {x:4*scale,z:4*scale}
-
-function onWindowResize(){
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x87CEEB, 1)
-
-document.body.appendChild(renderer.domElement)
-window.addEventListener('resize', onWindowResize, false);
+var startPos = {x:4*Game.Settings.Scale,z:4*Game.Settings.Scale}
 
 camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.z = 400;
 scene = new THREE.Scene();
 
-function Character(){
+function Character(position,properties,elevation){
 
 }
 
-function Projectile(properties){
+function Projectile(position,properties,elevation){
+  this.color = properties.color
+  if(!this.color) this.color = 0xFFFFFF
+  this.size = this.size
+  if(!this.size) this.size = 15
+
   this.projectile = new THREE.Mesh(
 
   )
 }
 
 function Level(layout,elevation){
+  this.elevation = elevation
   this.level = []
 	for(y=0;y<layout.length;y++){
 	  this.level.push([])
@@ -52,41 +41,41 @@ function Level(layout,elevation){
 	    //Create a wall if there is one
 	    if(layout[y][x].wall > 0){
 	      this.level[y][x].wall=new THREE.Mesh(
-	          GameGeometry.Wall,
-	          new THREE.MeshBasicMaterial({map: GameTextures[layout[y][x].wall-1]})
+	          Game.Geometry.Wall,
+	          new THREE.MeshBasicMaterial({map: Game.Textures[layout[y][x].wall-1]})
 	      )
-	      this.level[y][x].wall.position.x = scale * x
-        this.level[y][x].wall.position.z = scale * y
+	      this.level[y][x].wall.position.x = Game.Settings.Scale * x
+        this.level[y][x].wall.position.z = Game.Settings.Scale * y
 
-	      this.level[y][x].wall.position.y = elevation * scale
+	      this.level[y][x].wall.position.y = this.elevation * Game.Settings.Scale
 
 	      scene.add(this.level[y][x].wall)
 	      }
 
 	    if(layout[y][x].roof > 0){
 	      this.level[y][x].roof = new THREE.Mesh(
-	        GameGeometry.Surface,
-	        new THREE.MeshBasicMaterial({map: GameTextures[layout[y][x].roof-1]})
+	        Game.Geometry.Surface,
+	        new THREE.MeshBasicMaterial({map: Game.Textures[layout[y][x].roof-1]})
 	      )
 
-       this.level[y][x].roof.position.x = scale * x
-       this.level[y][x].roof.position.z = scale * y
+       this.level[y][x].roof.position.x = Game.Settings.Scale * x
+       this.level[y][x].roof.position.z = Game.Settings.Scale * y
        this.level[y][x].roof.rotation.x = -Math.PI / 2
 
-       this.level[y][x].roof.position.y = elevation * scale - (scale / 2)
+       this.level[y][x].roof.position.y = this.elevation * Game.Settings.Scale - (Game.Settings.Scale / 2)
 
        scene.add(this.level[y][x].roof)
 	    }
 	    if(layout[y][x].ground > 0){
 	      this.level[y][x].ground = new THREE.Mesh(
-	          GameGeometry.Surface,
-	          new THREE.MeshBasicMaterial({map: GameTextures[layout[y][x].ground-1]})
+	          Game.Geometry.Surface,
+	          new THREE.MeshBasicMaterial({map: Game.Textures[layout[y][x].ground-1]})
         )
-        this.level[y][x].ground.position.x = scale * x
-        this.level[y][x].ground.position.z = scale * y
+        this.level[y][x].ground.position.x = Game.Settings.Scale * x
+        this.level[y][x].ground.position.z = Game.Settings.Scale * y
         this.level[y][x].ground.rotation.x = Math.PI / 2
 
-        this.level[y][x].ground.position.y = elevation * scale + (scale / 2)
+        this.level[y][x].ground.position.y = this.elevation * Game.Settings.Scale + (Game.Settings.Scale / 2)
 
         scene.add(this.level[y][x].ground)
 	    }
@@ -94,13 +83,13 @@ function Level(layout,elevation){
 	}
 
   this.collision = function(oldPos,newPos){
-    if(this.level[Math.floor((newPos.z+scale/2)/scale)][Math.floor((oldPos.x+scale/2)/scale)].wall){
-      newPos.z = Math.floor(newPos.z/scale)*scale + scale/2
+    if(this.level[Math.floor((newPos.z+Game.Settings.Scale/2)/Game.Settings.Scale)][Math.floor((oldPos.x+Game.Settings.Scale/2)/Game.Settings.Scale)].wall){
+      newPos.z = Math.floor(newPos.z/Game.Settings.Scale)*Game.Settings.Scale + Game.Settings.Scale/2
       if(newPos.z > oldPos.z) newPos.z -= 0.001
       else if(newPos.z < oldPos.z) newPos.z += 0.001
     }
-    if(this.level[Math.floor((oldPos.z+scale/2)/scale)][Math.floor((newPos.x+scale/2)/scale)].wall){
-      newPos.x = Math.floor(newPos.x/scale) * scale + scale/2
+    if(this.level[Math.floor((oldPos.z+Game.Settings.Scale/2)/Game.Settings.Scale)][Math.floor((newPos.x+Game.Settings.Scale/2)/Game.Settings.Scale)].wall){
+      newPos.x = Math.floor(newPos.x/Game.Settings.Scale) * Game.Settings.Scale + Game.Settings.Scale/2
       if(newPos.x > oldPos.x) newPos.x -= 0.001
       else if(newPos.x < oldPos.x) newPos.x += 0.001
     }
--- textures.js
+++ textures.js
@@ -1,23 +1,52 @@
-var maxAnisotropy = renderer.capabilities.getMaxAnisotropy();
+var Game = {
+  Settings : {
+    Scale:100, //Size of 3d objects
+    MeshQuality:1,//1-10 scale (1 best)
+    Antialias:true //Uses antialias
+  },
+  Textures : [],
+  Sprites : []
+}
 
-var GameTextures = []
+Game.Geometry = {
+  Wall: new THREE.BoxBufferGeometry(Game.Settings.Scale, Game.Settings.Scale, Game.Settings.Scale),
+  Surface: new THREE.PlaneGeometry(Game.Settings.Scale, Game.Settings.Scale),
+  BulletXS: new THREE.SphereGeometry(Math.ceil(Game.Settings.Scale/Game.Settings.MeshQuality/7),Game.Settings.Scale/7,Game.Settings.Scale/7),
+  BulletS: new THREE.SphereGeometry(Math.ceil(Game.Settings.Scale/Game.Settings.MeshQuality/5),Game.Settings.Scale/5,Game.Settings.Scale/5),
+  BulletM: new THREE.SphereGeometry(Math.ceil(Game.Settings.Scale/Game.Settings.MeshQuality/3),Game.Settings.Scale/3,Game.Settings.Scale/3),
+  BulletL: new THREE.SphereGeometry(Math.ceil(Game.Settings.Scale/Game.Settings.MeshQuality/2),Game.Settings.Scale/2,Game.Settings.Scale/2),
+  BulletXL: new THREE.SphereGeometry(Math.ceil(Game.Settings.Scale/Game.Settings.MeshQuality/1.5),Game.Settings.Scale/1.5,Game.Settings.Scale/1.5),
+  SpriteXS: new THREE.PlaneGeometry(Game.Settings.Scale/3,Game.Settings.Scale/3),
+  SpriteS: new THREE.PlaneGeometry(Game.Settings.Scale/2,Game.Settings.Scale/2),
+  SpriteM: new THREE.PlaneGeometry(Game.Settings.Scale,Game.Settings.Scale),
+  SpriteL: new THREE.PlaneGeometry(Game.Settings.Scale*1.5,Game.Settings.Scale*1.5),
+  SpriteXL: new THREE.PlaneGeometry(Game.Settings.Scale*2,Game.Settings.Scale*2)
+}
+
+var renderer;
+
+function onWindowResize(){
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+renderer = new THREE.WebGLRenderer({antialias: Game.Settings.Antialias});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x87CEEB, 1)
+
+document.body.appendChild(renderer.domElement)
+window.addEventListener('resize', onWindowResize, false);
+
+Game.Settings.Antisotropy = renderer.capabilities.getMaxAnisotropy() //Texture Antisotropy
 
 for(a=0;a<24;a++){
-  GameTextures.push(new THREE.TextureLoader().load('Assets/Tiles/t_'+a+'.png'))
+  Game.Textures.push(new THREE.TextureLoader().load('Assets/Tiles/t_'+a+'.png'))
 }
 
-for(a=0;a<GameTextures.length;a++){
-  GameTextures[a].anisotropy = maxAnisotropy
+for(a=0;a<Game.Textures.length;a++){
+  Game.Textures[a].anisotropy = Game.Settings.Antisotropy
 }
 
-var GameSprites = []
-
-GameSprites.push(new THREE.TextureLoader().load('Assets/Monsters/skeleton0.png'))
-
-GameGeometry = {
-  Wall: new THREE.BoxBufferGeometry(scale, scale, scale),
-  Surface: new THREE.PlaneGeometry(scale, scale),
-  SpriteSmall: new THREE.PlaneGeometry(scale/2,scale/2),
-  SpriteNormal: new THREE.PlaneGeometry(scale,scale),
-  SpriteLarge: new THREE.PlaneGeometry(scale*2,scale*2)
-}
+Game.Sprites.push(new THREE.TextureLoader().load('Assets/Monsters/skeleton0.png'))
